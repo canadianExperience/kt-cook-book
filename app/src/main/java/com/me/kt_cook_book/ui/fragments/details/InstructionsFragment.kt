eda974cfcd15dbdc41d6c24f3534beb666113878
canadianExperience <lena2157@mail.ru>
2022-03-01 15:50:53 -0500

    details instructions fragment binded

--- app/src/main/java/com/me/kt_cook_book/ui/fragments/details/InstructionsFragment.kt
+++ app/src/main/java/com/me/kt_cook_book/ui/fragments/details/InstructionsFragment.kt
@@ -2,18 +2,28 @@ package com.me.kt_cook_book.ui.fragments.details
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.me.kt_cook_book.R
 import com.me.kt_cook_book.databinding.FragmentInstructionsBinding
+import com.me.kt_cook_book.viewmodels.DetailsViewModel
 
 class InstructionsFragment : Fragment(R.layout.fragment_instructions) {
     private var _binding: FragmentInstructionsBinding? = null
     private val binding  get() = _binding!!
+    private val detailsViewModel by viewModels<DetailsViewModel>(ownerProducer = { requireParentFragment() })
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         _binding = FragmentInstructionsBinding.bind(view)
+
+        val websiteUrl = detailsViewModel.result?.sourceUrl
+        websiteUrl?.let {
+            binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+            binding.instructionsWebView.loadUrl(websiteUrl)
+        }
     }
 
     override fun onDestroy() {
